0df2f9a415698c99925b2d06941d9bae4897762e
Jungbum Woo <woojungbum12@gmail.com>
2019-05-15 22:20:58 +0900

    inital commit

---
+++ init.js
@@ -0,0 +1,13 @@
+import "./db";
+import app from "./app";
+import dotenv from "dotenv";
+dotenv.config();
+import "./models/Video";
+
+
+const PORT = process.env.PORT || 4000;
+
+const handleListening = () =>
+  console.log(`Let's gi-gi-: http://localhost:${PORT}`);
+
+app.listen(PORT, handleListening);
