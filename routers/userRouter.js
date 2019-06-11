7356a569481af8b4f4e6bcff0c744c09f60bedf1
Jungbum Woo <woojungbum12@gmail.com>
2019-06-11 23:17:26 +0900

    0611

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -4,14 +4,16 @@ import {
   userDetail,
   editProfile,
   changePassword
-  // eslint-disable-next-line import/no-unresolved
 } from "../controllers/userController";
+import {
+  onlyPrivate
+} from "../middlewares";
 
 const userRouter = express.Router();
 
 
-userRouter.get(routes.userDetail, userDetail);
-userRouter.get(routes.editProfile, editProfile);
-userRouter.get(routes.changePassword, changePassword);
+userRouter.get(routes.editProfile, onlyPrivate, editProfile);
+userRouter.get(routes.changePassword, onlyPrivate, changePassword);
 userRouter.get(routes.userDetail(), userDetail);
+
 export default userRouter;
--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -10,23 +10,24 @@ import {
   // eslint-disable-next-line import/no-unresolved
 } from "../controllers/videoController";
 import {
-  uploadVideo
+  uploadVideo,
+  onlyPrivate
 } from "../middlewares";
 
 const videoRouter = express.Router();
 
 // Upload
-videoRouter.get(routes.upload, getUpload);
-videoRouter.post(routes.upload, uploadVideo, postUpload);
+videoRouter.get(routes.upload, onlyPrivate, getUpload);
+videoRouter.post(routes.upload, onlyPrivate, uploadVideo, postUpload);
 
 // Video Detail
 videoRouter.get(routes.videoDetail(), videoDetail);
 
 //  Edit Video
-videoRouter.get(routes.editVideo(), getEditVideo);
-videoRouter.post(routes.editVideo(), postEditVideo);
+videoRouter.get(routes.editVideo(), onlyPrivate, getEditVideo);
+videoRouter.post(routes.editVideo(), onlyPrivate, postEditVideo);
 
 //  Delte Video
-videoRouter.get(routes.deleteVideo(), deleteVideo);
+videoRouter.get(routes.deleteVideo(), onlyPrivate, deleteVideo);
 
 export default videoRouter;
